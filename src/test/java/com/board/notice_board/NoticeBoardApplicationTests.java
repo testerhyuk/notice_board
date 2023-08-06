5c2611fd2520787340d1b640c2e57aea570b9e30
Hyuk <79625509+testerhyuk@users.noreply.github.com>
2023-08-06 14:37:39 +0900

    first commit

---
+++ src/test/java/com/board/notice_board/NoticeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.notice_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
